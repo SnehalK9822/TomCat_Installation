afaf9d45aa8e1341e03cccb9db8818d80548aa98
SnehalK9822 <snehalkumbhar7083@gmail.com>
2024-12-31 20:14:07 +0530

    Update install_apache.sh

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt-get update -y
+
+# Install Apache HTTP Server
+sudo apt-get install apache2 -y
+
+# Enable and start Apache service
+sudo systemctl enable apache2
+sudo systemctl start apache2
+
+echo "Apache HTTP Server installed successfully."
 
